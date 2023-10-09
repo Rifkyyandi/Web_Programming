37925391b6728cd9d20e0abf5a7fed5b57d53daa
Rifkyyandi <122662185+Rifkyyandi@users.noreply.github.com>
2023-10-09 15:34:04 +0700

    Add files via upload

---
+++ Praktikum-1/Persegi.php
@@ -0,0 +1,46 @@
+<?php
+//Variabel
+$s = 20;
+
+//Rumus Menghitung Keliling Persegi
+$keliling = 4 * $s;
+
+//Rumus Menghitung Luas Persegi
+$luas = $s * $s;
+
+//Tampilan
+Print "<center><hr><h1>Menghitung Keliling Dan Luas Persegi <h1></center>";
+Print "<center><h4>Rifki Pramayandi Mahesa - 210511156 - TI21D <h4><hr></center>";
+print "<br/>";
+
+print "=================================";
+print "<br/>";
+print "Diketuahui";
+print "<br/>";
+print "Sisi : ".$s;
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "Rumus Menghitung Luas Persegi";
+print "<br/>";
+print "Luas = Sisi * Sisi";
+print "<br/>";
+print "Luas Persegi Adalah = ".$luas;
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "Rumus Menghitung Keliling Persegi";
+print "<br/>";
+print "Keliling = 4 * Sisi";
+print "<br/>";
+print "Keliling Persegi Adalah = ".$keliling;
+print "<br/>";
+print "=================================";
+print "<br/>";
+?>
---
+++ Praktikum-1/Trapesium.php
@@ -0,0 +1,58 @@
+<?php
+//Variabel
+$a = 2;
+$b = 4;
+$c = 12;
+$d = 7;
+$t = 2;
+
+//Rumus Menghitung Keliling Trapesium
+$keliling = $a + $b + $c + $d;
+
+//Rumus Menghitung Luas Trapesium
+$luas = ($a + $b / 2) * $t;
+
+//Tampilan
+Print "<center><hr><h1>Menghitung Keliling Dan Luas Trapesium <h1></center>";
+Print "<center><h4>Rifki Pramayandi Mahesa - 210511156 - TI21D <h4><hr></center>";
+print "<br/>";
+
+print "=================================";
+print "<br/>";
+print "Diketuahui";
+print "<br/>";
+print "a = ".$a;
+print "<br/>";
+print "b = ".$b;
+print "<br/>";
+print "c = ".$c;
+print "<br/>";
+print "d = ".$d;
+print "<br/>";
+print "t = ".$t;
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "Rumus Menghitung Luas Trapesium";
+print "<br/>";
+print "Luas = (a + b / 2) * t";
+print "<br/>";
+print "Luas Trapesium Adalah = ".$luas;
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "<br/>";
+print "=================================";
+print "<br/>";
+print "Rumus Menghitung Keliling Trapesium";
+print "<br/>";
+print "Keliling = a + b + c + d";
+print "<br/>";
+print "Keliling Trapesium Adalah = ".$keliling;
+print "<br/>";
+print "=================================";
+print "<br/>";
+?>
